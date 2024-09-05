64fa1382d862b091804d1df2b7cbad135afe40ff
alendavies <alendavies10@gmail.com>
2024-09-05 15:58:54 -0300

    errors

--- ej-individual/src/errors.rs
+++ ej-individual/src/errors.rs
@@ -1,15 +1,7 @@
 #[derive(Debug)]
 pub enum SqlError {
-    InvalidTable,       // relacionado a problemas con el procesamiento de las tablas.
-    InvalidColumn,      // relacionado a problemas con el procesamiento de columnas.
-    InvalidSyntax,      // relacionado a problemas con el procesamiento de consultas.
-    Error(CustomError), // tipo genérico para otros posibles errores detectados.
-}
-
-#[derive(Debug)]
-pub enum CustomError {
-    ReaderError,
-    WriteError,
-    FileError,
-    HashError,
+    InvalidTable,  // relacionado a problemas con el procesamiento de las tablas.
+    InvalidColumn, // relacionado a problemas con el procesamiento de columnas.
+    InvalidSyntax, // relacionado a problemas con el procesamiento de consultas.
+    Error,         // tipo genérico para otros posibles errores detectados.
 }

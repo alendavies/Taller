8752cb22252078e9a98ca271f0f2bbb6627b2337
alendavies <alendavies10@gmail.com>
2024-09-07 14:06:03 -0300

    fix execute to use conditions

--- ej-individual/src/clauses/where_sql.rs
+++ ej-individual/src/clauses/where_sql.rs
@@ -1,8 +1,6 @@
-use std::{collections::HashMap, panic::Location};
-
+use super::{condition::Condition, recursive_parser::parse_condition};
 use crate::{errors::SqlError, utils::is_where};
-
-use super::recursive_parser::{parse_condition, Condition, LogicalOperator, Operator};
+use std::collections::HashMap;
 
 pub struct Where {
     pub condition: Condition,
@@ -15,41 +13,11 @@ impl Where {
         }
         let mut pos = 1;
         let condition = parse_condition(&tokens, &mut pos)?;
-        println!("{:?}", condition);
 
         Ok(Where { condition })
     }
 
     pub fn execute(&self, register: &HashMap<String, String>) -> Result<bool, SqlError> {
-        let op_result: Result<bool, SqlError> = match &self.condition {
-            Condition::Simple {
-                field,
-                operator,
-                value,
-            } => {
-                let y = value;
-                if let Some(x) = register.get(field) {
-                    return match operator {
-                        Operator::Lesser => Ok(x < y),
-                        Operator::Greater => Ok(x > y),
-                        Operator::Equal => Ok(x == y),
-                        Operator::Unknown => Err(SqlError::Error),
-                    };
-                } else {
-                    return Err(SqlError::Error);
-                }
-            }
-            Condition::Complex {
-                left,
-                operator,
-                right,
-            } => match operator {
-                LogicalOperator::Not => todo!(),
-                LogicalOperator::Or => todo!(),
-                LogicalOperator::And => todo!(),
-            },
-        };
-
-        op_result
+        return self.condition.execute(register);
     }
 }

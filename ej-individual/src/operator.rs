e6b86a93d61d0d82ef164d4fd8bc2c376374e999
alendavies <alendavies10@gmail.com>
2024-09-09 10:46:42 -0300

    operators docs

--- ej-individual/src/operator.rs
+++ ej-individual/src/operator.rs
@@ -1,3 +1,8 @@
+/// Enum for the operators used in the queries.
+/// - `Equal`: Equal operator
+/// - `Greater`: Greater than operator
+/// - `Lesser`: Lesser than operator
+
 #[derive(Debug, PartialEq)]
 pub enum Operator {
     Equal,

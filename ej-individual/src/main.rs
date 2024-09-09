fc78b45da06e58a83d7a4875d0b14221570190c7
alendavies <alendavies10@gmail.com>
2024-09-08 21:06:56 -0300

    modularizacion

--- ej-individual/src/main.rs
+++ ej-individual/src/main.rs
@@ -2,88 +2,14 @@ mod clauses;
 mod errors;
 mod register;
 mod table;
+mod tokens;
 mod utils;
 
 use clauses::{delete_sql::Delete, insert_sql::Insert, select_sql::Select, update_sql::Update};
 use errors::SqlError;
 use std::env;
+use tokens::tokens_from_query;
 use utils::table_to_csv;
-
-fn tokens_from_query(string: &str) -> Vec<String> {
-    let mut index = 0;
-    let mut tokens = Vec::new();
-    let mut current = String::new();
-
-    let string = string.replace(";", "");
-    let length = string.len();
-
-    let mut char = string.chars().nth(index).unwrap_or('0');
-
-    while index < length {
-        if char.is_alphabetic() || char == '_' {
-            while (char.is_alphabetic() || char == '_') && index < length {
-                current.push(char);
-                index += 1;
-                char = string.chars().nth(index).unwrap_or('0');
-            }
-            tokens.push(current);
-            current = String::new();
-        } else if char.is_numeric() {
-            while char.is_numeric() && index < length {
-                current.push(char);
-                index += 1;
-                char = string.chars().nth(index).unwrap_or('0');
-            }
-
-            tokens.push(current);
-            current = String::new();
-        } else {
-            if char.is_whitespace() || char == ',' {
-                index += 1;
-                char = string.chars().nth(index).unwrap_or('0');
-            } else if char == '\'' {
-                index += 1;
-                char = string.chars().nth(index).unwrap_or('0');
-
-                while char != '\'' && index < length {
-                    current.push(char);
-                    index += 1;
-                    char = string.chars().nth(index).unwrap_or('0');
-                }
-
-                tokens.push(current);
-                current = String::new();
-                index += 1;
-                char = string.chars().nth(index).unwrap_or('0');
-            } else if char == '(' {
-                index += 1;
-                char = string.chars().nth(index).unwrap_or('0');
-
-                while char != ')' && index < length {
-                    current.push(char);
-                    index += 1;
-                    char = string.chars().nth(index).unwrap_or('0');
-                }
-
-                tokens.push(current);
-                current = String::new();
-                index += 1;
-                char = string.chars().nth(index).unwrap_or('0');
-            } else {
-                while !char.is_alphanumeric() && !char.is_whitespace() && index < length {
-                    current.push(char);
-                    index += 1;
-                    char = string.chars().nth(index).unwrap_or('0');
-                }
-                tokens.push(current);
-                current = String::new();
-            }
-        }
-    }
-    tokens.retain(|s| !s.is_empty());
-
-    tokens
-}
 
 fn exec_query(folder_path: &str, query: &str) -> Result<Vec<String>, SqlError> {
     let tokens = tokens_from_query(query);
